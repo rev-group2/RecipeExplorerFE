cda1b4869a4cf1bf24573b1eff60d95b40505605
Ian-Hoole <hoolei@sonoma.edu>
2024-10-22 13:52:36 -0700

    profile page can now be edited and updates the user context

--- src/components/Profile/ProfileController.tsx
+++ src/components/Profile/ProfileController.tsx
@@ -1,19 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import ProfileView from './ProfileView';
+import React, { useContext, useEffect, useState } from 'react';
+import ProfileView, { ProfileViewType } from './ProfileView';
 import { Params, useParams } from 'react-router-dom';
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import path from 'path';
-import { User } from '../Context/UserContext';
+import { User, UserContext } from '../Context/UserContext';
 
 const config = require("../../config");
 const URL = `${config.path}`;
 
 
 
-export default function ProfileController(props: any) {
+export default function ProfileController(props: any): JSX.Element {
 
+    const setUser: Function | undefined = props.setUser;
+    const user:User | undefined= useContext(UserContext);
     const { id }:Params<string> = useParams()
-    const [profile, setProfile] = useState<User>();
+    const [profile, setProfile] = useState<ProfileViewType>();
+
+    const isUserProfile = user && profile && user.uuid === profile.uuid;
 
     useEffect(() => {
         async function getUser(id: string | undefined){
@@ -31,16 +35,33 @@ export default function ProfileController(props: any) {
                 }
             }
         }
-
         getUser(id)
     }, [id])
 
 
-    function updateProfile(){
+    async function updateProfile(editedUser:ProfileViewType):Promise<boolean>{
+        console.log(editedUser)
         console.log("BUTTON PUSH YAY");
+        try{
+            const header: AxiosRequestConfig = { headers: { Authorization: `Bearer ${user?.token}` } };
+            const response: AxiosResponse = await axios.post(`${URL}/users/profile`, editedUser, header);
+            console.log(response);
+            if(response.status >= 200 && response.status < 300){
+                setProfile(editedUser);
+                if(setUser){
+                    setUser({...user, ...editedUser});
+                }
+                
+                return true;
+            }
+            return false;
+        }catch(error){
+            console.log(error)
+            return false;
+        }
     }
 
     return (
-        <ProfileView profile={profile} updateProfile={updateProfile}/>
+        <ProfileView profile={profile} token={user?.token} isUserProfile={isUserProfile} updateProfile={updateProfile}/>
     )
 }
--- src/components/Profile/ProfileView.tsx
+++ src/components/Profile/ProfileView.tsx
@@ -4,40 +4,52 @@ import { User, UserContext } from '../Context/UserContext';
 
 export type ProfileViewType = {
     username: string,
-    password: string,
+    email: string,
     picture: string,
-    description: string
+    description: string,
+    uuid: string
 }
 
-export default function ProfileView(props:any) {
+export default function ProfileView(props:any): JSX.Element {
 
-    const user = useContext(UserContext);
-    const profile:User = props.profile;
     
+    const profile: ProfileViewType = props.profile;
+    const isUserProfile = props.isUserProfile;
     const [editing, setEditing] = useState<boolean>(false);
-    
+    const [editedProfile, setEditedProfile] = useState<ProfileViewType>();
+
     function changeEditing(event: any){
+        if(!editing){
+            setEditedProfile(profile);
+        }
         setEditing(!editing);
     }
 
-    function changeProfile(event: any){
-        props.updateProfile()
+    async function changeProfile(event: any){
+        if(await props.updateProfile(editedProfile)){
+            setEditing(!editing);
+            console.log("after everything");
+            console.log(profile);
+        }
+        else{
+            console.log("error updating resource");
+        }
     }
 
-    if(profile?.uuid && editing){
+    if(profile && editing){
         return <div>
-            <h1><input></input>{profile.username}</h1>
+            <h1><input type="text" defaultValue={`${profile.username}`} placeholder='username' id="usernameChange" onChange={(e: any) => editedProfile && setEditedProfile({ ...editedProfile, username: e.target.value })}/></h1>
             <button className="btn btn-primary mx-3" onClick={changeEditing}>Finish Editing</button>
             <img id="picture" src={(profile.picture || "No Picture Located")} alt={"profile image"}/>
-            <h3>{profile.email}</h3>
-            <p>{profile.description || "No Description added"}</p>
+            <h3><input type="text" defaultValue={`${profile.email}`} placeholder='email' id="emailChange" onChange={(e: any) => editedProfile && setEditedProfile({ ...editedProfile, email: e.target.value })}/></h3>
+            <p><input type="text" defaultValue={`${profile.description || "Edit your description here"}`} placeholder='description' id="descriptionChange" onChange={(e: any) => editedProfile && setEditedProfile({ ...editedProfile, description: e.target.value })} /></p>
             <button className="btn btn-primary mx-3" onClick={changeProfile}>SAVE</button>
             </div >
     }
-    else if(profile?.uuid){
+    else if(profile){
         return <div>
             <h1>{profile.username}</h1>
-            {(profile.uuid === user?.uuid) && <button className="btn btn-primary mx-3" onClick={changeEditing}>Edit Page</button>}
+            {isUserProfile && <button className="btn btn-primary mx-3" onClick={changeEditing}>Edit Page</button>}
             <img id="picture" src={(profile.picture || "No Picture Located")} alt={"profile image"}/>
             <h3>{profile.email}</h3>
             <p>{profile.description || "No Description added"}</p>
