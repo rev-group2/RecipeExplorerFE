9b00b463f3a54dab764c4f33d637d835b2c95138
Burak <burakadmr@gmail.com>
2024-10-24 06:55:17 -0700

    Edit recipe (#1)
    * Renamed file
    
    * Changed import
    
    * Changed import
    
    * Renamed file
    
    * Passing props to controller
    
    * Edit recipe functionality
    
    * Conditionally loading data if editing recipe
    
    * Removed function parameter
    
    * Removed console log
    
    * Moved function call
    
    * Delete recipe functionality
    
    * Changed default value logic on inputs and edit recipe button text

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import MyProfileController from './components/MyProfile/MyProfileController';
 import NavBar from './components/NavBar/NavBar';
 import {User, UserContext} from "./components/Context/UserContext"
 import LoginController from './components/Login/LoginController';
-import CreateRecipeController from './components/CreateRecipe/CreateRecipeController';
+import RecipeController from './components/Recipe/RecipeController';
 import RegisterController from './components/Register/RegisterController';
 
 function App() : JSX.Element {
@@ -21,7 +21,7 @@ function App() : JSX.Element {
         <Route path="/" element={<HomeController />}/>
         <Route path="/register" element={<RegisterController/>}/>
         <Route path="/login" element={<LoginController setUser={setUser} />} />
-        <Route path='/create-recipe' element={<CreateRecipeController />}/>
+        <Route path='/create-recipe' element={<RecipeController recipeUuid={undefined} isEditing={false} />}/>
       </Routes>
       </UserContext.Provider>
     </div>
