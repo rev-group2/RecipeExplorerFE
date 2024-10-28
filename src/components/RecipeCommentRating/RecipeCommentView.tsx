eb29f6cd81396f92253c39f6d7092d9df842e11f
Burak <burakadmr@gmail.com>
2024-10-28 06:37:01 -0700

    Testing recipe comment rating (#13)
    * Spacing
    
    * Comment submission logic
    
    * Form to submit a comment
    
    * Getting recipe, rating, and comments
    
    * Displaying recipe details
    
    * Recipe details styling
    
    * Rating display
    
    * rating display
    
    * comment input width
    
    * calling function passed in as prop from parent when comment submitted
    
    * function to get comments when comment is submitted in child
    
    * passing function prop through child
    
    * Showing more comment information
    
    * Changed comments to reviews
    
    * Comment styling
    
    * Text align style rule
    
    * Changed name
    
    * Showing option to comment only if user hasn't commented on recipe
    
    * Checking if user has commented on recipe
    
    * Passing prop to check if user has commented
    
    * Styling comment button
    
    * Resetting state values
    
    * Moved files into separate folder
    
    * Changed import
    
    * Checking if api responses are ok
    
    * Home tests
    
    * Home test script
    
    * Calling randomIndex in else
    
    * Renamed folder
    
    * Updated directory in import
    
    * Added script for testing recipecommentrating
    
    * RecipeCommentRating tests
    
    * Added testid data attributes used for testing form

--- src/components/RecipeCommentRating/RecipeCommentView.tsx
+++ src/components/RecipeCommentRating/RecipeCommentView.tsx
@@ -5,14 +5,14 @@ type FormProps = {submitForm: (e: React.FormEvent<HTMLFormElement>) => void}
 function RecipeCommentView({submitForm}: FormProps) {
   return (
     <>
-    <form id="comment-form" onSubmit={(e: React.FormEvent<HTMLFormElement>) => submitForm(e)}>
+    <form id="comment-form" data-testid="comment-form" onSubmit={(e: React.FormEvent<HTMLFormElement>) => submitForm(e)}>
       <label>
-        <textarea placeholder='Leave a review' id='comment-input' name='description' rows={2} cols={63} required></textarea>
+        <textarea placeholder='Leave a review' id='comment-input' data-testid="comment-input" name='description' rows={2} cols={63} required></textarea>
       </label>
       <label>Rate (1 - 10)
-        <input type="number" id="recipe-rating" name="rating" min="1" max="10" required/>
+        <input type="number" id="recipe-rating" data-testid="recipe-rating" name="rating" min="1" max="10" required/>
       </label>
-      <button id="comment-button" type='submit'>Review</button>
+      <button id="comment-button" data-testid="comment-button" type='submit'>Review</button>
     </form>
     </>
   )
